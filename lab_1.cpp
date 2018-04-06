7bdf98886b2f4ef7654adaa08f045c5cee30c3db
Diego Charrez <dcharrezt@unsa.edu.pe>
2018-04-05 19:00:06 -0500

    mpi example

---
+++ lab_1.cpp
@@ -0,0 +1,28 @@
+/*
+ * lab_1.cpp
+ * Copyright (C) 2018 Diego Charrez <dcharrezt@unsa.edu.pe>
+ *
+ * Distributed under terms of the  license.
+ */
+
+// - Implementar y comparar los 2-bucles anidados FOR presentados en el cap. 2 del libro, pag 22.
+
+// - Implementar en C/C++ la multiplicación de matrices clásica, la versión de tres bucles anidados y evaluar su desempeño considerando el tamaño de las matrices.
+// - Implementar la version por bloques, seis bloques anidados, evaluar su desempeño y compararlo con la multiplicación de matrices clásica.
+// - Ejecutar ambos algoritmos paso a paso, y analisar el movimiento de datos entre la memoria principal y la memoria cache. Hacer una evaluación de acuerdo a la complejidad algoritmica.
+// - Ejecutar ambos algoritmos utilizando las herramientas valgrind y kcachegrind para obtener una evaluación mas precisa de su desempeño en términos de cache misses.
+
+#include "lab_1.h"
+
+
+#include <iostream>
+
+void first_pair_loops();
+void second_pair_loops();
+
+
+int main(){
+
+
+    return 0;
+}
---
+++ mpi_vector.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <mpi.h>
+#define TAM 1000
+
+int main(int argc, char **argv){
+	int i, my_rank, procs, part, ind=0;
+	int vet[TAM];
+	int tag=10;
+	MPI_Status status;
+
+	int st = 0;
+	int tmp;
+	int a;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &procs);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+	part = (int)(TAM / (procs-1));
+
+	if(my_rank == 0) {
+		for (i = 0; i < TAM; ++i)
+		{	
+			vet[i] = i;
+		}
+		for (i = 1; i < procs; i++)
+		{
+			MPI_Send(&vet[ind],part,MPI_INT,i,tag,MPI_COMM_WORLD);
+			ind = ind + part;
+		}
+
+		for (i = 1; i < procs; i++)
+		{
+			MPI_Recv( &a , 1 ,MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG ,MPI_COMM_WORLD, &status);
+			st += a;
+		
+		}
+
+		printf("%d \n",st);
+
+	}
+	else{
+		MPI_Recv(vet,part,MPI_INT,0,tag,MPI_COMM_WORLD,&status);
+		//printf("Proc.%d recibe [ ", miRank);
+		int s = 0;
+		for(i = 0;i < part; i++) {
+			s += vet[i];
+		}
+
+		printf("%d\n", s);
+
+		MPI_Send( &s , 1,MPI_INT,0,tag,MPI_COMM_WORLD);
+
+	}
+}
